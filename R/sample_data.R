c4833e987120ace11be0a1f8ebf57351b82050a0
Ruben Vine <ruben.vine@phs.scot>
2023-08-23 13:36:40 +0100

    fix up sample_data roxygen to get rid of warnings

--- R/sample_data.R
+++ R/sample_data.R
@@ -7,13 +7,12 @@
 #' @format ## `sample_data`
 #' A dataframe with 1,008 rows and 5 columns:
 #'
-#' @usage data("sample_data")
+#'
 #' \describe{
 #'   \item{health_board}{Health Board Name}
 #'   \item{simd}{Social Index of Multiple Deprivation Quintile (includes Total)}
 #'   \item{date}{YYYY-MM-DD, first of the month for all of 2022}
 #'   \item{number}{randomly generated numbers (using `sample()`)}
-#'   \item{proportion}{Proportion of number within each SIMD quintile by
-#'   date and health board}
+#'   \item{proportion}{Proportion of number within each SIMD quintile by date and health board}
 #'   }
 "sample_data"
