dcbebd27ff89259d00614f3e7194f793e8117be2
Ruben Vine <ruben.vine@phs.scot>
2023-08-23 10:50:26 +0100

    defining global variables for suppression functions

---
+++ R/globals.R
@@ -0,0 +1,8 @@
+
+# suppression_flag - Used for primary suppression
+utils::globalVariables(c("suppression_flag",
+                         # Primary suppression
+                         "orig",
+                         # Secondary suppression
+                         "abs_number",
+                         "row_n"))
